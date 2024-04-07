15677a49de7963968e4275d2b7510a8691559a1a
Dev <dev.ace21@sot.pdpu.ac.in>
2024-04-07 18:03:32 +0530

    mongodb added to backend

--- server/api/index.js
+++ server/api/index.js
@@ -1,6 +1,30 @@
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
 const express = require("express")
 const app = express()
 const port = 5000
+const mongoose = require('mongoose');
+
+mongoose.set("strictQuery", true);
+
+const options ={
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+
+const dbUrl = process.env.MONGODB_URI_PROD || "mongodb://127.0.0.1:27017/wordle"
+// Connect to MongoDB
+mongoose
+  .connect(dbUrl,options)
+  .then(() => {
+    console.log("Data base Connection Open!!");
+  })
+  .catch(() => {
+    console.log("Error in Connecting Data Base");
+  });
+
+
 
 app.get("/", (req, res) => {
     res.send("Hello from sever")
